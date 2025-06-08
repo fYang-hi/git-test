61865ea2050e9184f9029b071a90a2a6177a804f
fy135 <fyang@fyangdeMacBook-Pro.local>
2025-06-08 11:08:41 +0800

    second commit

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -7,7 +7,7 @@ public class Test {
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            System.out.println("result = " + i);
         }
     }
 }
