85b39462d85fd5937f8f94a5269d57e6c408d689
fYang-hi <81908297+fYang-hi@users.noreply.github.com>
2025-06-08 11:30:17 +0800

    Update Test.java
    third commit

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -1,7 +1,5 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Test {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
@@ -9,5 +7,7 @@ public class Test {
         for (int i = 1; i <= 5; i++) {
             System.out.println("result = " + i);
         }
+
+        System.out.printf("This a test project!");
     }
 }
